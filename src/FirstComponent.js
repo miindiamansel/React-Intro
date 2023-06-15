046757231014a97dcaf38124935ee910f7b45b28
miindia.mansel <miindia.mansel@gmail.com>
2023-06-15 13:55:49 -0700

    first react component

---
+++ src/FirstComponent.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class FirstComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>beach coders!</h1>
+            </div>
+        )
+    }
+}
+export default FirstComponent 
